5cd00afee6ee080e66a2c6b0ccd515c156f8950c
Agusmat22 <118922239+Agusmat22@users.noreply.github.com>
2023-09-04 14:57:12 -0300

    Add files via upload

---
+++ Ejercicios/Ejercicio16/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Entidades;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Area del circulo: "+CalculadoraDeArea.CalcularAreaCirculo(5));
+
+            Console.WriteLine("Area del triangulo: " + CalculadoraDeArea.CalcularAreaTriangulo(5,10));
+
+            Console.WriteLine("Area del cuadrado: " + CalculadoraDeArea.CalcularAreaCuadrado(20));
+        }
+    }
+}
