46063716fcf14a4dc5221d87f076d4598fd04eb7
Agusmat22 <agusgarcia569@gmail.com>
2023-09-15 02:57:58 -0300

    actualizado

--- TpIntegrador/TpIntegrador/Entidades/Operacion.cs
+++ TpIntegrador/TpIntegrador/Entidades/Operacion.cs
@@ -2,9 +2,11 @@
 {
     public class Operacion
     {
+        //atributos del tipo Numeracion
         private Numeracion primerOperando;
         private Numeracion segundoOperando;
 
+        //Inicializo los atributos
         public Operacion(Numeracion primerOperando, Numeracion segundoOperando)
         {
             this.primerOperando = primerOperando;
