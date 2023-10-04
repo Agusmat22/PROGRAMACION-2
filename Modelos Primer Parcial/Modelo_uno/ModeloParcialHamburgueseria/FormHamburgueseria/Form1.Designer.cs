eb8b0d0bf12c61ae239332e43f2ddb16b8f98d1b
Agusmat22 <agusgarcia569@gmail.com>
2023-10-04 16:45:47 -0300

    update

--- Modelos Primer Parcial/Modelo_uno/ModeloParcialHamburgueseria/FormHamburgueseria/Form1.Designer.cs
+++ Modelos Primer Parcial/Modelo_uno/ModeloParcialHamburgueseria/FormHamburgueseria/Form1.Designer.cs
@@ -28,12 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            lstHamburguesas = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            groupBox1 = new GroupBox();
+            comboBox1 = new ComboBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            comboBox2 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            richTextBox1 = new RichTextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Dni del cliente";
+            textBox1.Size = new Size(118, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // lstHamburguesas
+            // 
+            lstHamburguesas.FormattingEnabled = true;
+            lstHamburguesas.ItemHeight = 15;
+            lstHamburguesas.Location = new Point(12, 74);
+            lstHamburguesas.Name = "lstHamburguesas";
+            lstHamburguesas.Size = new Size(215, 79);
+            lstHamburguesas.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(176, 17);
+            label1.TabIndex = 2;
+            label1.Text = "Seleccione su hamburguesa";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(77, 175);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 17);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(69, 99);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 17);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(285, 74);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(223, 157);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ingredientes";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(15, 119);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(168, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(18, 19);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(94, 19);
+            radioButton1.TabIndex = 6;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(18, 44);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 19);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(12, 193);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(215, 23);
+            comboBox2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(233, 193);
+            button1.Name = "button1";
+            button1.Size = new Size(28, 23);
+            button1.TabIndex = 9;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(189, 119);
+            button2.Name = "button2";
+            button2.Size = new Size(28, 23);
+            button2.TabIndex = 10;
+            button2.Text = "+";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 242);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(496, 147);
+            richTextBox1.TabIndex = 10;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(522, 401);
+            Controls.Add(richTextBox1);
+            Controls.Add(button1);
+            Controls.Add(comboBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lstHamburguesas);
+            Controls.Add(textBox1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private ListBox lstHamburguesas;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private GroupBox groupBox1;
+        private Button button2;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Button button1;
+        private RichTextBox richTextBox1;
     }
 }
